46b2c3e5984bd27b3b7bab4cae120974ed8b8ff0
Andy Stabler <andy_60@hotmail.co.uk>
2015-08-06 16:37:42 +0100

    Update user model to produce FinanceRows for balance data

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,52 +13,51 @@ class User < ActiveRecord::Base
   end
 
   ##
-  # Let's start by creating a row for every day, but there's no reason we couldn't let the user specify
-  # the unit of time they wish each row to represent (day/week/month, etc)
+  # creates a list of FinanceRows based on saved balance updates
   #
-  def finance_rows_between(start_date, end_date)
-    days = (end_date.to_date - start_date.to_date).to_i
+  def balance_data_between(start_date, end_date)
+    # pointless going any further if there are no balance updates associated with this user
+    return nil unless balances.any?
+
+    relevant_balances = balances.where(on: start_date..end_date+1)
+    # no balances for these dates
+    return nil unless relevant_balances.any?
+
     rows = Array.new
-    days.times do |i|
-      day = start_date+i.days #+0 days, +1 day, +2 days, etc.
-      # get latest (with respect to start_date) balance for this user
-
-      #balance
-
-
-      #rows.add FinanceRow.new
+    balance_groups = relevant_balances.group_by { |bal| bal.on.day_s } #(&:day_of_datetime)
+    balance_groups.each do |day, bals|
+      row_on = bals.last.on.day_s
+      row_bal = bals.sort_by { |bal| bal[:on] }.last
+      row_month_diff = bals.last.last_of_month? ? bals.last.month_diff : nil
+      rows << FinanceRow.new(row_on, row_bal.value, row_bal.diff_from_previous_balance, row_month_diff)
     end
+    rows
+  end
+
+  def all_balance_data
+    bals = balances.order(:on)
+
+    return nil unless bals.any?
+    from = bals.first.on
+    to = bals.last.on
+    balance_data_between(from, to)
   end
 
   ##
-  # Returns the balance for the user on a given date
+  # creates a list of FinanceRows based on calculations from saved transfers
   #
-  # If the date is in the future, then the incoming and outgoings are used to
-  # predict the balance the user will have at that time
-  #
-  # If the date is not in the future, then the current balance associated with
-  # this user is returned
-  def balance_on in_date
-
-    if in_date.today?
-      logger.debug "date(#{in_date}) is today"
-      return balance
-    else
-      logger.debug "date(#{in_date}) is NOT today"
-    end
-
-    # get all Transfers where date is >= today <= in_date
-    trans = incoming.where('dat > ? AND dat <= ?', DateTime.now, in_date)
-    trans.each do |tr|
-      # tr.
-    end
+  def prediction_data_between(start_date, end_date)
+    # map from date
+    trans_date_map = Hash.new
 
   end
 
-  private
-
-  def days_between start_date, end_date
-
+  def all_prediction_data
+    trans = transfers.order(:on)
+    return nil unless trans.any?
+    from = trans.first.on
+    to = trans.last.on
+    prediction_data_between(from, to)
   end
 
 end
