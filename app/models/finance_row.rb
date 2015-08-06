186bddd259b144f43dc0e8b06a5369fec1612847
Andy Stabler <andy_60@hotmail.co.uk>
2015-08-06 16:33:02 +0100

    Add FinanceRow - what the user will see in each row of the finance table

--- app/models/finance_row.rb
+++ app/models/finance_row.rb
@@ -1,13 +1,11 @@
 class FinanceRow
 
-  attr_accessor :on, :in_val, :out_val, :balance, :diff, :running_monthly_diff
+  attr_accessor :on, :balance, :diff, :running_monthly_diff
 
-  def initialize(on, in_val, out_val, balance, running_monthly_diff)
+  def initialize(on, balance, difference, running_monthly_diff)
     @on = on
-    @in_val = in_val
-    @out_val = out_val
     @balance = balance
-    @diff = @in_val -@out_val
+    @diff = difference
     @running_monthly_diff = running_monthly_diff
   end
 end
