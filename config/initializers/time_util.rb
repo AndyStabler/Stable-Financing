42db70cf6c6faa300fd52441cc23aa394f623a31
Andy Stabler <andy_60@hotmail.co.uk>
2015-08-16 14:23:28 +0100

    Add months_between function to time_util.rb

--- config/initializers/time_util.rb
+++ config/initializers/time_util.rb
@@ -2,4 +2,8 @@ class Time
   def day_s
     self.to_date.to_s(:db)
   end
+
+  def months_between(in_date)
+    (in_date.year - self.year) * 12 + in_date.month - self.month - (in_date.day >= self.day ? 0 : 1)
+  end
 end
