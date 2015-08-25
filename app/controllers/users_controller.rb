14213af10f0655b965e1d698e11f2d8ba51b883c
Andy Stabler <andy_60@hotmail.co.uk>
2015-08-25 16:21:35 +0100

    Add dummy action to users controller for updating balance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,6 +77,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def update_balance
+    respond_to do |format|
+      format.html { redirect_to :back, notice: 'Balance was successfully added.' }
+      format.json { render json: :no_content }
+    end
+  end
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_user
