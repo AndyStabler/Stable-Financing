c548a66d5ebec8faf551ada4a2cfbe603f04fce7
Andy Stabler <andy_60@hotmail.co.uk>
2015-09-02 15:13:56 +0100

    Update Transfer model to include validation

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -3,4 +3,8 @@ class Transfer < ActiveRecord::Base
 
   enum recurrence: [:no, :daily, :weekly, :monthly]
 
+  validates :amount, :user, :on, :recurrence, presence: true
+  validates :outgoing, inclusion: [true, false]
+  validates :amount, numericality: true
+
 end
