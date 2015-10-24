4a4eeb888bbe6bec6a47149d24db400b9cc9ecf2
Andy Stabler <andy_60@hotmail.co.uk>
2015-10-24 18:57:44 +0100

    Update schema so users table knows about username and password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818164429) do
+ActiveRecord::Schema.define(version: 20150902170206) do
 
   create_table "balances", force: :cascade do |t|
     t.decimal  "value"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20150818164429) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "username",   null: false
+    t.string   "password"
+    t.string   "email"
   end
 
 end
