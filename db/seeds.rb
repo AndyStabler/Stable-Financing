891312adf211455187f654e77f31c7b2aa62fd10
Andy Stabler <andy_60@hotmail.co.uk>
2015-10-25 16:20:16 +0000

    update seeds to include more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.delete_all
-users = [{name: 'Andy Stabler', username: "andystabler"}, {name: 'Fiona Barron', username: "fionabarron"}]
+users = [{name: 'Andy Stabler', username: "andystabler", email: "andy_60@hotmail.co.uk", password: "$2a$10$X2KpieEghC49Qes3lPI/oOi32qBtHJLaEU3Fot6wP2.BNOs7NNFSK"},
+         {name: 'Fiona Barron', username: "fionabarron", email: "fionabarron@hotmail.co.uk", password: "$2a$10$X2KpieEghC49Qes3lPI/oOi32qBtHJLaEU3Fot6wP2.BNOs7NNFSK"}]
 User.create!(users)
 
 Transfer.delete_all
@@ -21,18 +22,11 @@ transfers = [{on: Date.today - 5.days, reference: 'init', amount: 3000.00, recur
              {on: Date.today, reference: 'electricity', amount: 35, recurrence: 3, user: User.find_by_name("Andy Stabler"), outgoing: true},
              {on: Date.today, reference: 'water', amount: 41, recurrence: 3, user: User.find_by_name("Andy Stabler"), outgoing: true},
              {on: Date.today, reference: 'internet', amount: 24, recurrence: 3, user: User.find_by_name("Andy Stabler"), outgoing: true},
-             {on: Date.today, reference: 'car', amount: 200.00 , recurrence: :monthly, user: User.find_by_name("Andy Stabler"), outgoing: true},
-             {on: Date.today+1.month, reference: 'Council Tax', amount: 1200 , recurrence: :no, user: User.find_by_name("Andy Stabler"), outgoing: true},
-             {on: Date.today, reference: 'Rent', amount: 390 , recurrence: :monthly, user: User.find_by_name("Andy Stabler"), outgoing: true},
+             {on: Date.today, reference: 'car', amount: 200.00, recurrence: :monthly, user: User.find_by_name("Andy Stabler"), outgoing: true},
+             {on: Date.today+1.month, reference: 'Council Tax', amount: 1200, recurrence: :no, user: User.find_by_name("Andy Stabler"), outgoing: true},
+             {on: Date.today, reference: 'Rent', amount: 390, recurrence: :monthly, user: User.find_by_name("Andy Stabler"), outgoing: true},
              {on: Date.today, reference: 'wages', amount: 1602.48, recurrence: 3, user: User.find_by_name("Andy Stabler"), outgoing: false}]
 
-# transfers = [{on: Date.today, reference: 'car', amount: '50.25', recurrence: 1, user: User.find_by_name("Fiona Barron"), outgoing: true},
-#              {on: Date.today, reference: 'mobile', amount: '24.50', recurrence: 2, user: User.find_by_name("Fiona Barron"), outgoing: true},
-#              {on: Date.today, reference: 'mobile', amount: '30', recurrence: 0, user: User.find_by_name("Fiona Barron"), outgoing: true},
-#              {on: Date.today, reference: 'rent', amount: '390', recurrence: 2, user: User.find_by_name("Fiona Barron"), outgoing: true},
-#              {on: Date.today, reference: 'rent', amount: '390', recurrence: 1, user: User.find_by_name("Fiona Barron"), outgoing: true},
-#              {on: Date.today, reference: 'FreeAgent', amount: '1200', recurrence: 2, user: User.find_by_name("Fiona Barron"), outgoing: false},
-#              {on: Date.today, reference: 'Parents', amount: '400', recurrence: 0, user: User.find_by_name("Fiona Barron"), outgoing: false}]
 Transfer.create!(transfers)
 
 Balance.delete_all
@@ -44,6 +38,3 @@ Balance.create!([{id: 1, value: 500, on: Date.today-5.weeks, user: User.find_by_
                  {id: 6, value: 1000, on: Date.today-2, user: User.find_by_name("Andy Stabler")},
                  {id: 7, value: 3000, on: Date.today-10.days, user: User.find_by_name("Fiona Barron")},
                  {id: 8, value: 2900, on: Date.today-5.days, user: User.find_by_name("Fiona Barron")}])
-# {value: 2946, on: Date.today-4, user: User.find_by_name("Andy Stabler")},
-# {value: 3000, on: Date.today-2, user: User.find_by_name("Andy Stabler")},
-# {value: 100, on: Date.today, user: User.find_by_name("Andy Stabler")}])
