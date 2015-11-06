71e81992916d34630b5010340afb377090020c58
Andy Stabler <andy_60@hotmail.co.uk>
2015-11-06 23:12:22 +0000

    Migrate db to use password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902170206) do
+ActiveRecord::Schema.define(version: 20151106230425) do
 
   create_table "balances", force: :cascade do |t|
     t.decimal  "value"
@@ -51,10 +51,10 @@ ActiveRecord::Schema.define(version: 20150902170206) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "username",   null: false
-    t.string   "password"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "username",        null: false
+    t.string   "password_digest"
     t.string   "email"
   end
 
