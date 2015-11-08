b4e44597153df84d3a3d12f875a42c486234a3f9
Andy Stabler <andy_60@hotmail.co.uk>
2015-11-08 20:46:34 +0000

    Add logout integration test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -19,8 +19,28 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with valid information" do
     get login_path
     post login_path, :session => { :username => @user.email, :password => "pwd" }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+  end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, :session => { :username => @user.email, :password => "pwd" }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", login_path
   end
 end
