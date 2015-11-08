43e0ff31c2d475e055bb56f483b63bb6ca9f5f1b
Andy Stabler <andy_60@hotmail.co.uk>
2015-11-08 20:37:44 +0000

    Add logout and fix current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,8 +4,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   def current_user
-    @current_user ||= User.find(id: session[:user_id])
+    @current_user ||= User.find_by(:id => session[:user_id])
   end
 
   def logged_in?
