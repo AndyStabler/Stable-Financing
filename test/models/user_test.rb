4a844e2ecd0f996b5778d50536887e41e017e08d
Andy Stabler <andy_60@hotmail.co.uk>
2015-11-08 00:18:55 +0000

    Use new authentication method

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,30 +6,13 @@ class UserTest < ActiveSupport::TestCase
     @user = users(:andy)
   end
 
-  ##### Login ####
-
-  test "authenticate valid username and valid password user not nil" do
-  	username = "andystabler"
-  	password = "pwd"
-  	assert !User.authenticate(username, password).nil?
+  ##### Authentication ####
+  test "authenticate valid password user not nil" do
+    assert !!@user.authenticate("pwd")
   end
 
-  test "authenticate invalid user and password exists user nil" do
-  	username = "aabler"
-  	password = "pwd"
-  	assert User.authenticate(username, password).nil?
-  end
-
-  test "authenticate invalid user and password does not exist user nil" do
-  	username = "andysasdfsdtabler"
-  	password = "pasdfwd"
-  	assert User.authenticate(username, password).nil?
-  end
-
-  test "authenticate valid user and password does not exist user nil" do
-  	username = "andystabler"
-  	password = "padfwd"
-  	assert User.authenticate(username, password).nil?
+  test "authenticate invalid password user nil" do
+    assert !@user.authenticate("asdf")
   end
 
   #### Other tests here ####
