5e6541b9e3c7647a4c69ca4adbf95a94ae81609d
Andy Stabler <andy_60@hotmail.co.uk>
2015-11-11 19:52:36 +0000

    Add login functionality for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,24 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 
   def is_logged_in?
-  	!session[:user_id].nil?
+    !session[:user_id].nil?
   end
+
+  def log_in_as(user, options = {})
+    password = options[:password] || "pwd"
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+        post login_path, :session => { :username => user.username, 
+                                        :password => password, 
+                                        :remember_me => remember_me }
+    else
+        session[:user_id] = user.id
+    end
+  end
+
+  private 
+
+    def integration_test?
+        defined?(post_via_redirect)
+    end
 end
