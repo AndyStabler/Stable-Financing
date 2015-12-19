57c7555e35af8d5d482b34a716099023641ec32f
Andy Stabler <andy_60@hotmail.co.uk>
2015-12-19 15:05:56 +0000

    store the date instead of time in the finance_item

--- app/models/finance_item.rb
+++ app/models/finance_item.rb
@@ -3,7 +3,7 @@ class FinanceItem
   attr_accessor :on, :balance, :diff, :running_monthly_diff
 
   def initialize(on, balance, difference, running_monthly_diff)
-    @on = on
+    @on = on.to_date
     @balance = balance
     @diff = difference
     @running_monthly_diff = running_monthly_diff
