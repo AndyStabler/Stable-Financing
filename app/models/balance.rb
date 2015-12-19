346fbee1a6c434902aad89fa24d52f0fb71c1625
Andy Stabler <andy_60@hotmail.co.uk>
2015-12-19 11:49:10 +0000

    Add diff_from_previous_day
    Also tidy things up and add comments

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,7 +1,7 @@
 class Balance < ActiveRecord::Base
   belongs_to :user
 
-  validates :value, :on, :user, presence: {message: "Value cannot be empty"}
+  validates :value, :on, :user, presence: { message: "Value cannot be empty" }
   validates :value, numericality: true
 
   def last_of_month?
@@ -9,10 +9,29 @@ class Balance < ActiveRecord::Base
     new_ordered_bals[on.beginning_of_month].sort_by { |bal| bal.on }.last == self
   end
 
+  # Returns the difference between this value and the previous day's
+  def diff_from_previous_day
+    bals = user.balances.order(:on)
+    ind = bals.index self
+
+    # if it's the first balance return the balance value
+    return value if ind == 0
+    # group the balances by the day they occur on
+    date_bals = user.balances.group_by { |b| b.on.to_date }
+    # get the balances that have an on date less than the current balance's
+    previous_balance = date_bals.select { |date, bals| date < on.to_date }.sort_by {|date, bals| date}
+    # if there are no balances with an on date less than the current balance, return the current balance's value
+    return value if previous_balance == nil
+    # previous_balance is an ordered Hash. Get the last hash item as an array, then get the last element (the hash value- an array of balances)
+    # order these balances, and get the last's value
+    value - previous_balance.last.last.sort_by {|b| b.on}.last.value
+  end
+
+  # Returns the difference between this balance and the previous one (might be on the same day)
+  # they could both occur on the same day
   def diff_from_previous_balance
     bals = user.balances.order(:on)
-    ind = bals.index self #bals[self]
-    # if it's the first in the list, just return the balance value
+    ind = bals.index self
     return value unless ind != 0
     # if the month is different to the previous balance's, then the diff is 0
     # previous balance is from same month? diff is balance - old balance
--- test/models/balance_test.rb
+++ test/models/balance_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class BalanceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    test "diff_from_previous_day when value greater than previous day" do
+      expected = balances(:previous_day_3).value - balances(:previous_day_2).value
+      assert_equal expected, balances(:previous_day_3).diff_from_previous_day
+    end
+
+    test "diff_from_previous_day when value less than previous day" do
+      expected = balances(:previous_day_4).value - balances(:previous_day_2).value
+      assert_equal expected.to_f, balances(:previous_day_4).diff_from_previous_day.to_f
+    end
+
 end
