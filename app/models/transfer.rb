409a9f8abdc61378abebb98bbc0823d4fc3f09c1
Andy Stabler <andy_60@hotmail.co.uk>
2015-12-22 00:31:35 +0000

    Add group_transfers_by_date class method to Transfers class
    Didn't really belong in NumberCruncher.rb

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -23,4 +23,36 @@ class Transfer < ActiveRecord::Base
   def self.sum_outgoing(transfers)
     all_outgoing(transfers).map(&:amount).inject(:+) || 0.0
   end
+
+  # Group transfers by the day they will take place on
+  # A transfer can be registered once, but may have a recurrence that means it will occur repeatedly (every mnonth say)
+  #
+  # Returns a hash mappping dates to the transfers that occur
+  def self.group_transfers_by_date transfers, start_date, end_date
+    return {} unless transfers.any? && end_date >= start_date
+
+    date_to_transfers = {}
+
+    transfers.each do |trans|
+      on = trans.on.to_date
+      recurrence = trans.recurrence.to_sym
+      if recurrence == :no && (on >= start_date && on <= end_date)
+        (date_to_transfers[on]||=[]) << trans
+      elsif recurrence == :daily && on <= end_date
+        ((end_date - on).to_i + 1).times do |day_to_add|
+          (date_to_transfers[on + day_to_add.days]||=[]) << trans
+        end
+      elsif recurrence == :weekly && on <= end_date
+        (((end_date - on) / 7).to_i + 1).times do |wk|
+          (date_to_transfers[on + wk.weeks]||=[]) << trans
+        end
+      elsif recurrence == :monthly && on <= end_date
+        ((on.months_between(end_date)).to_i + 1).times do |mt|
+          (date_to_transfers[on + mt.months]||=[]) << trans
+        end
+      end
+    end
+    date_to_transfers
+  end
+
 end
