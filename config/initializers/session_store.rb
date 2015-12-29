0dcd132a86e8c8b269f581435b65ee9ae1aca9aa
Andy Stabler <andy_60@hotmail.co.uk>
2015-12-29 15:43:20 +0000

    Update rails version to 4.2.5

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_Stable_Financing_session'
+Rails.application.config.session_store :cookie_store, key: '_stable_financing_session'
