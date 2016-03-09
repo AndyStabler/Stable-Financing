63205e1e74a5379999bdaf5efa8cc9607fa51062
Andy Stabler <andy_60@hotmail.co.uk>
2016-03-09 21:58:44 +0000

    Remove authentication tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,20 +6,4 @@ class UserTest < ActiveSupport::TestCase
     @user = users(:andy)
   end
 
-  ##### Authentication ####
-  test "authenticate valid password user not nil" do
-    assert !!@user.authenticate("pwd")
-  end
-
-  test "authenticate invalid password user nil" do
-    assert !@user.authenticate("asdf")
-  end
-
-  test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
-  end
-
-  #### Other tests here ####
-
-
 end
