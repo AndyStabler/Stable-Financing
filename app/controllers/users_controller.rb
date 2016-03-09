35e6db9bb39fac254268bab00f71473b1de8e88b
Andy Stabler <andy_60@hotmail.co.uk>
2016-03-09 21:13:30 +0000

    Remove create action - handled by Devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,24 +21,6 @@ class UsersController < ApplicationController
   def edit
   end
 
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
-    balance = Balance.new({:value => 0.0, :on => Time.zone.now, :user => @user})
-    respond_to do |format|
-      if @user.save && balance.save
-        log_in @user
-        format.html { redirect_to @user, notice: 'Welcome to the party!' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        balance.destroy
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
