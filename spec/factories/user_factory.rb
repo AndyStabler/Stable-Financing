cd74a71a747cd6de4e80f0c74db7aaec28b4300f
Andy Stabler <andy_60@hotmail.co.uk>
2016-03-13 17:56:52 +0000

    put homer into a trait

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,15 +2,11 @@ FactoryGirl.define do
 
   factory :user do | user |
 
-    user.name "Homer Simpson"
-    user.email "homer@aol.com"
-    user.password "password"
-    user.password_confirmation "password"
-
-    trait :with_empty_balance do
-      after(:create) do |user|
-        create(:balance, :empty, user: user)
-      end
+    trait :homer do
+      user.name "Homer Simpson"
+      user.email "homer@aol.com"
+      user.password "password"
+      user.password_confirmation "password"
     end
   end
 
