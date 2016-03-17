1fd75b5fbba43e965d67fa39ae62058a6c29e96a
Andy Stabler <andy_60@hotmail.co.uk>
2016-03-17 22:22:04 +0000

    Don't include transfers occurring before start_date

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -54,7 +54,9 @@ class Transfer < ActiveRecord::Base
       elsif recurrence == :monthly && on <= end_date
         months = (on.months_between(end_date)).to_i + 1
         months.times do |mt|
-          (date_to_transfers[on + mt.months]||=[]) << trans
+          date_of_transfer = on + mt.months
+          next if date_of_transfer < start_date
+          (date_to_transfers[date_of_transfer]||=[]) << trans
         end
       end
     end
