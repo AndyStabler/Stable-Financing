cd78c2f863a3a2ded152480abd5c400a992eff42
Andy Stabler <andy_60@hotmail.co.uk>
2016-03-19 00:18:45 +0000

    Add methods to test

---
+++ spec/models/number_cruncher_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe NumberCruncher, type: :model do
+
+  describe "#calculate_balance_forecast" do
+  end
+
+  describe "#trans_per_month" do
+  end
+
+  describe "#balance_forecast_between" do
+  end
+
+  describe "#finance_log_between" do
+  end
+end
