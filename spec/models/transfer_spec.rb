525643e63ad341b88a9d02101a036776c374bfe1
Andy Stabler <andy_60@hotmail.co.uk>
2016-03-22 23:00:45 +0000

    Update transfer specs based on refactorings - pt 1

--- spec/models/transfer_spec.rb
+++ spec/models/transfer_spec.rb
@@ -6,98 +6,103 @@ RSpec.describe Transfer, type: :model do
     FactoryGirl.create(:user, :homer)
   end
 
-  describe ".all_incoming" do
+  let(:transfer_calculator) do
+    TransferCalculator.new(homer)
+  end
+
+  describe "#incoming" do
+
     context "when there are no incoming transfers" do
       it "should be empty" do
-        expect(Transfer.all_incoming(homer.transfers)).to be_empty
+        expect(transfer_calculator.incoming).to be_empty
       end
     end
 
     context "when there are only incoming transfers" do
       it "should return all transfers" do
         expected_transfers = []
-        expected_transfers << FactoryGirl.create(:transfer, :incoming, :daily, user: homer, reference: "transfer 1", on: DateTime.now, amount: 20.0)
-        expected_transfers << FactoryGirl.create(:transfer, :incoming, :daily, user: homer, reference: "transfer 2", on: DateTime.now, amount: 20.0)
-        expected_transfers << FactoryGirl.create(:transfer, :incoming, :daily, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
-        expect(Transfer.all_incoming(homer.transfers)).to eql expected_transfers
+        expected_transfers << FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 1", on: DateTime.now, amount: 20.0)
+        expected_transfers << FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 2", on: DateTime.now, amount: 20.0)
+        expected_transfers << FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
+        expect(transfer_calculator.incoming).to eql expected_transfers
       end
     end
 
     context "when there are a mix of incoming and outgoing transfers" do
       it "should return only the incoming transfers" do
        expected_transfers = []
-       expected_transfers << FactoryGirl.create(:transfer, :incoming, :daily, user: homer, reference: "transfer 1", on: DateTime.now, amount: 20.0)
-       expected_transfers << FactoryGirl.create(:transfer, :incoming, :daily, user: homer, reference: "transfer 2", on: DateTime.now, amount: 20.0)
-       expected_transfers << FactoryGirl.create(:transfer, :incoming, :daily, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
-       FactoryGirl.create(:transfer, :outgoing, :daily, user: homer, reference: "transfer 4", on: DateTime.now, amount: 20.0)
-       FactoryGirl.create(:transfer, :outgoing, :daily, user: homer, reference: "transfer 5", on: DateTime.now, amount: 20.0)
-       FactoryGirl.create(:transfer, :outgoing, :daily, user: homer, reference: "transfer 6", on: DateTime.now, amount: 20.0)
-       expect(Transfer.all_incoming(homer.transfers)).to eql expected_transfers
+       expected_transfers << FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 1", on: DateTime.now, amount: 20.0)
+       expected_transfers << FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 2", on: DateTime.now, amount: 20.0)
+       expected_transfers << FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
+       FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 4", on: DateTime.now, amount: 20.0)
+       FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 5", on: DateTime.now, amount: 20.0)
+       FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 6", on: DateTime.now, amount: 20.0)
+       expect(transfer_calculator.incoming).to eql expected_transfers
      end
    end
  end
 
- describe ".sum_incoming" do
+ describe "#total_incoming" do
     context "when there are no transfers" do
       it "should return 0" do
-        expect(Transfer.sum_incoming(homer.transfers)).to be_zero
+        expect(transfer_calculator.total_incoming).to be_zero
       end
     end
 
     context "when there are some transfers" do
       it "should return the total" do
-        FactoryGirl.create(:transfer, :incoming, :daily, user: homer, reference: "transfer 1", on: DateTime.now, amount: 20.0)
-        FactoryGirl.create(:transfer, :incoming, :daily, user: homer, reference: "transfer 2", on: DateTime.now, amount: 20.0)
-        FactoryGirl.create(:transfer, :incoming, :daily, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
-        expect(Transfer.sum_incoming(homer.transfers)).to eql(60)
+        FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 1", on: DateTime.now, amount: 20.0)
+        FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 2", on: DateTime.now, amount: 20.0)
+        FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
+        expect(transfer_calculator.total_incoming).to eql(60)
       end
     end
   end
 
-  describe "all_outgoing" do
+  describe "#outgoing" do
     context "when there are no outgoing transfers" do
       it "should be empty" do
-        expect(Transfer.all_outgoing(homer.transfers)).to be_empty
+        expect(transfer_calculator.outgoing).to be_empty
       end
     end
 
     context "when there are only outgoing transfers" do
       it "should return all transfers" do
         expected_transfers = []
-        expected_transfers << FactoryGirl.create(:transfer, :outgoing, :daily, user: homer, reference: "transfer 1", on: DateTime.now, amount: 20.0)
-        expected_transfers << FactoryGirl.create(:transfer, :outgoing, :daily, user: homer, reference: "transfer 2", on: DateTime.now, amount: 20.0)
-        expected_transfers << FactoryGirl.create(:transfer, :outgoing, :daily, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
-        expect(Transfer.all_outgoing(homer.transfers)).to eql expected_transfers
+        expected_transfers << FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 1", on: DateTime.now, amount: 20.0)
+        expected_transfers << FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 2", on: DateTime.now, amount: 20.0)
+        expected_transfers << FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
+        expect(transfer_calculator.outgoing).to eql expected_transfers
       end
     end
 
     context "when there are a mix of outgoing and incoming transfers" do
       it "should return only the outgoing transfers" do
         expected_transfers = []
-        FactoryGirl.create(:transfer, :incoming, :daily, user: homer, reference: "transfer 1", on: DateTime.now, amount: 20.0)
-        FactoryGirl.create(:transfer, :incoming, :daily, user: homer, reference: "transfer 2", on: DateTime.now, amount: 20.0)
-        FactoryGirl.create(:transfer, :incoming, :daily, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
-        expected_transfers << FactoryGirl.create(:transfer, :outgoing, :daily, user: homer, reference: "transfer 4", on: DateTime.now, amount: 20.0)
-        expected_transfers << FactoryGirl.create(:transfer, :outgoing, :daily, user: homer, reference: "transfer 5", on: DateTime.now, amount: 20.0)
-        expected_transfers << FactoryGirl.create(:transfer, :outgoing, :daily, user: homer, reference: "transfer 6", on: DateTime.now, amount: 20.0)
-        expect(Transfer.all_outgoing(homer.transfers)).to eql expected_transfers
+        FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 1", on: DateTime.now, amount: 20.0)
+        FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 2", on: DateTime.now, amount: 20.0)
+        FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
+        expected_transfers << FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 4", on: DateTime.now, amount: 20.0)
+        expected_transfers << FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 5", on: DateTime.now, amount: 20.0)
+        expected_transfers << FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 6", on: DateTime.now, amount: 20.0)
+        expect(transfer_calculator.outgoing).to eql expected_transfers
       end
     end
   end
 
-  describe ".sum_outgoing" do
+  describe "#total_outgoing" do
     context "when there are no transfers" do
       it "should return 0" do
-        expect(Transfer.sum_incoming(homer.transfers)).to be_zero
+        expect(transfer_calculator.total_incoming).to be_zero
       end
     end
 
     context "when there are some transfers" do
       it "should return the total" do
-        FactoryGirl.create(:transfer, :outgoing, :daily, user: homer, reference: "transfer 1", on: DateTime.now, amount: 20.0)
-        FactoryGirl.create(:transfer, :outgoing, :daily, user: homer, reference: "transfer 2", on: DateTime.now, amount: 20.0)
-        FactoryGirl.create(:transfer, :outgoing, :daily, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
-        expect(Transfer.sum_outgoing(homer.transfers)).to eql(60)
+        FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 1", on: DateTime.now, amount: 20.0)
+        FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 2", on: DateTime.now, amount: 20.0)
+        FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
+        expect(transfer_calculator.total_outgoing).to eql(60)
       end
     end
   end
@@ -164,7 +169,7 @@ RSpec.describe Transfer, type: :model do
         to_date = Date.today + 2.month
         transfer = FactoryGirl.create(:transfer, :incoming, :monthly, user: homer, reference: "transfer 1", on: Date.today-1.month-6.days, amount: 20)
         date_transfers = Transfer.group_transfers_by_date(homer.transfers, from_date, to_date)
-        total_months = (transfer.on.months_between(to_date)).to_i + 1
+        total_months = (Util::DateTimeUtil.months_between(transfer.on, to_date)).to_i + 1
         month_count = 0
         total_months.times { |i| month_count+=1 if (from_date..to_date).cover?(transfer.on + i.months) }
 
