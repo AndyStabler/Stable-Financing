5fce69093da231a9c7acb89cda8edb78757584fa
Andy Stabler <andy_60@hotmail.co.uk>
2016-03-23 22:43:48 +0000

    Add forecast for transfer daily

--- app/models/transfer_daily.rb
+++ app/models/transfer_daily.rb
@@ -1,6 +1,19 @@
 class TransferDaily < Transfer
 
-  def forecast
-    puts "forecasting daily transfer"
+  def forecast(from = nil, to)
+    transfer_occurences = []
+    from ||= on.to_date
+
+    return transfer_occurences unless to >= from && on.to_date <= to
+    transfer_date = on.to_date
+
+    days = Util::DateTime.days_between_inclusive(transfer_date, to)
+    days.times do |day_to_add|
+      new_transfer_date = transfer_date + day_to_add.days
+      next if new_transfer_date < from
+      transfer_occurences << new_transfer_date
+    end
+    transfer_occurences
   end
+
 end
