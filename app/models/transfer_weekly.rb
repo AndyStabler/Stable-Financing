5d3e88a25e4b5d7eacb83dcfce44aaeb166b1035
Andy Stabler <andy_60@hotmail.co.uk>
2016-03-23 23:08:26 +0000

    Add forecast fore weekly transfers

--- app/models/transfer_weekly.rb
+++ app/models/transfer_weekly.rb
@@ -1,6 +1,18 @@
 class TransferWeekly < Transfer
 
-  def forecast
-    puts "forecasting monthly transfer"
+  def forecast(from = nil, to)
+    transfer_occurences = []
+    from ||= on.to_date
+
+    return transfer_occurences unless to >= from && on.to_date <= to
+    transfer_date = on.to_date
+
+    weeks = Util::DateTime.weeks_between_inclusive(transfer_date, to)
+    weeks.times do |wk|
+      new_transfer_date = transfer_date + wk.weeks
+      next if new_transfer_date < from
+      transfer_occurences << new_transfer_date
+    end
+    transfer_occurences
   end
 end
