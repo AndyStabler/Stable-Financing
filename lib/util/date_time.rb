90d5fdb2c81981561208db8cb0a945761348916b
Andy Stabler <andy_60@hotmail.co.uk>
2016-03-23 22:43:00 +0000

    Kill redundant util in filename

---
+++ lib/util/date_time.rb
@@ -0,0 +1,17 @@
+module Util::DateTime
+  class << self
+    def months_between(from, to)
+      (to.year - from.year) * 12 + to.month - from.month - (to.day >= from.day ? 0 : 1)
+    end
+
+    # Public: calculates the number of inclusive days between two dates
+    #
+    # Examples
+    #
+    #   days_between_inclusve(1, 5)
+    #   # => 5
+    def days_between_inclusive(from, to)
+      (to - from).to_i + 1
+    end
+  end
+end
--- lib/util/date_time_util.rb
+++
@@ -1,7 +0,0 @@
-class Util::DateTimeUtil
-  class << self
-    def months_between(from, to)
-      (to.year - from.year) * 12 + to.month - from.month - (to.day >= from.day ? 0 : 1)
-    end
-  end
-end
