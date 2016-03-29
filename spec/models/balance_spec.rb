bb7477e86d329283eca45d7581a1d02af5f43c4d
Andy Stabler <andy_60@hotmail.co.uk>
2016-03-29 23:16:22 +0100

    Update tests to account for BST
    Weird behaviour with saying beginning_of_month. It seemed to go to an
    hour before the beginning_of_month...?

--- spec/models/balance_spec.rb
+++ spec/models/balance_spec.rb
@@ -94,49 +94,60 @@ RSpec.describe Balance, type: :model do
       end
 
       context "when there are no balances for the previous month" do
+        before(:each) do
+          # get rid of the initial balance of 0 that occurs on the day the user is created
+          homer.balances.destroy_all
+        end
+
         it "should calculate the difference correctly" do
-          FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month, user: homer, :value => 20.0)
+          # put the initial balance of 0 back in, but give it a consistent date
+          FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month+1.hour, user: homer, :value => 0.0)
+          FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month+2.hour, user: homer, :value => 20.0)
           FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month+1.day, user: homer, :value => 50.0)
           FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month+2.days, user: homer, :value => 5.0)
-          # There is an initial balance of 0 that has an :on value of Time.zone.now
-          # We remove this so we can do a simple check that the calculation is correct
-          # (having a value of 0 at any point in the month makes this calculation difficult)
-          homer.balances = homer.balances.reject { |b| b.value == BigDecimal.new(0) }
           expected_month_diff = 5.0
           expect(homer.balance.month_diff).to eql expected_month_diff
         end
 
         context "and there are balances for the next month" do
           it "should calculate the diff and ignore the next month's balances" do
-            FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month, user: homer, :value => 100.0)
+            # put the initial balance of 0 back in, but give it a consistent date
+            FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month+1.hour, user: homer, :value => 0.0)
+            FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month+2.hours, user: homer, :value => 100.0)
             FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month+1.day, user: homer, :value => 20.0)
             FactoryGirl.create(:balance, on: DateTime.now.end_of_month+1.day, user: homer, :value => 20.0)
-            homer.balances = homer.balances.reject { |b| b.value == BigDecimal.new(0) }
             expected_month_diff = 20.0
-            expect(homer.balances.first.month_diff).to eql expected_month_diff
+            expect(homer.balances.all.first.month_diff).to eql expected_month_diff
           end
         end
       end
 
       context "when there are balances for previous month" do
+        before(:each) do
+          # get rid of the initial balance of 0 that occurs on the day the user is created
+          homer.balances.destroy_all
+        end
+
         it "should calculate the diff based on last balance of previous month" do
-          FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month-1.day, user: homer, :value => 5.0)
-          FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month, user: homer, :value => 100.0)
+          # put the initial balance of 0 back in, but give it a consistent date
+          FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month-1.day, user: homer, :value => 0.0)
+          FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month-1.day+1.hour, user: homer, :value => 5.0)
+          FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month+1.hour, user: homer, :value => 100.0)
           FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month+1.day, user: homer, :value => 20.0)
-          homer.balances = homer.balances.reject { |b| b.value == BigDecimal.new(0) }
           expected_month_diff = 15.0
           expect(homer.balance.month_diff).to eql expected_month_diff
         end
 
         context "and there are balances for the next month" do
           it "should calculate the diff and ignore the next month's balances" do
-            FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month-1.day, user: homer, :value => 5.0)
-            FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month, user: homer, :value => 100.0)
+            # put the initial balance of 0 back in, but give it a consistent date
+            FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month-1.day, user: homer, :value => 0.0)
+            FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month-1.day + 1.hour, user: homer, :value => 5.0)
+            FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month+1.hour, user: homer, :value => 100.0)
             FactoryGirl.create(:balance, on: DateTime.now.beginning_of_month+1.day, user: homer, :value => 20.0)
             FactoryGirl.create(:balance, on: DateTime.now.end_of_month+1.day, user: homer, :value => 20.0)
-            homer.balances = homer.balances.reject { |b| b.value == BigDecimal.new(0) }
             expected_month_diff = 15.0
-            expect(homer.balances[1].month_diff).to eql expected_month_diff
+            expect(homer.balances.all[2].month_diff).to eql expected_month_diff
           end
         end
       end
@@ -144,3 +155,4 @@ RSpec.describe Balance, type: :model do
 
   end
 end
+
