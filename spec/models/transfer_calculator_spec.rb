8b217d0e0ab3d7bbbf6a675c8b878c5160d9cf3d
Andy Stabler <andy_60@hotmail.co.uk>
2016-04-01 14:16:39 +0100

    Add spec for transfers_occurring_on

--- spec/models/transfer_calculator_spec.rb
+++ spec/models/transfer_calculator_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe TransferCalculator, type: :model do
    end
  end
 
- describe "#total_incoming" do
+  describe "#total_incoming" do
     context "when there are no transfers" do
       it "should return 0" do
         expect(transfer_calculator.total_incoming).to be_zero
@@ -104,6 +104,19 @@ RSpec.describe TransferCalculator, type: :model do
         FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 3", on: DateTime.now, amount: 20.0)
         expect(transfer_calculator.total_outgoing).to eql(60)
       end
+    end
+  end
+
+  describe "#transfers_occurring_on" do
+    it "should be the transfers occurring on this day" do
+      on = DateTime.now
+      FactoryGirl.create(:transfer_daily, :incoming, user: homer, reference: "transfer 1", on: on - 1.day, amount: 20.0)
+      FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 2", on: on, amount: 20.0)
+      FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 3", on: on, amount: 20.0)
+      FactoryGirl.create(:transfer_daily, :outgoing, user: homer, reference: "transfer 4", on: on + 1.day, amount: 20.0)
+      occurring = transfer_calculator.transfers_occurring_on Date.today
+      expect(occurring.count).to be 3
+      expect(occurring.map(&:reference).sort == ["transfer 1", "transfer 2", "transfer 3"])
     end
   end
 end
