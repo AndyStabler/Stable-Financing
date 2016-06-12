363d01c8b3d3a809da470a6550f21057e52baba0
Andy Stabler <andy_60@hotmail.co.uk>
2016-06-12 13:43:32 +0100

    Reference class recurrence method

--- app/assets/javascripts/components/transfers.js.jsx
+++ app/assets/javascripts/components/transfers.js.jsx
@@ -24,7 +24,7 @@ var Transfers = React.createClass({
             reference={transfer.reference}
             on={new Date(transfer.on).toString()}
             amount={transfer.amount}
-            recurrence={transfer.recurrence}
+            recurrence={transfer.class.recurrence}
             id={transfer.id} />
         })}
       </div>
