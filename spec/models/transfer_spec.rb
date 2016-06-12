28c4977c40d9c053512f0dbbca70e1c7ccf373d0
Andy Stabler <andy_60@hotmail.co.uk>
2016-06-12 18:50:24 +0100

    Add recurrence spec to transfer

--- spec/models/transfer_spec.rb
+++ spec/models/transfer_spec.rb
@@ -6,11 +6,20 @@ RSpec.describe Transfer, type: :model do
     FactoryGirl.create(:user, :homer)
   end
 
+  let(:transfer) do
+    FactoryGirl.create(:transfer, :incoming, user: homer, reference: "transfer 1", on: DateTime.current-10.days, amount: 20)
+  end
   describe "#forecast" do
     it "should fail" do
-      t = FactoryGirl.create(:transfer, :incoming, user: homer, reference: "transfer 1", on: DateTime.current-10.days, amount: 20)
-      expect{t.forecast(Date.yesterday, Date.tomorrow)}.to raise_error(NotImplementedError)
+      # transfer = FactoryGirl.create(:transfer, :incoming, user: homer, reference: "transfer 1", on: DateTime.current-10.days, amount: 20)
+      expect{transfer.forecast(Date.yesterday, Date.tomorrow)}.to raise_error(NotImplementedError)
     end
   end
 
+  describe "#recurrence" do
+    it "should fail" do
+      # transfer = FactoryGirl.create(:transfer, :incoming, user: homer, reference: "transfer 1", on: DateTime.current-10.days, amount: 20)
+      expect {transfer.recurrence}.to raise_error(NotImplementedError)
+    end
+  end
 end
