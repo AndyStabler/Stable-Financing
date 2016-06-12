736e74f048cf28d50c6e53415f4a0d5ad72dff15
Andy Stabler <andy_60@hotmail.co.uk>
2016-06-12 10:01:25 +0100

    Refactor status check and add transfers specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,17 +6,18 @@ RSpec.describe UsersController, type: :controller do
     FactoryGirl.create(:user, :homer)
   end
 
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in homer
+  end
+
   describe "GET show" do
-    before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-      sign_in homer
-    end
 
     context "when the format is html" do
       it "should render the html show template" do
         get :show,:id => homer.id
         expect(response).to render_template(:show)
-        expect(response.status).to be 200
+        check_status response
       end
     end
 
@@ -25,17 +26,52 @@ RSpec.describe UsersController, type: :controller do
         get :show, :id => homer.id, :format => "csv"
         expect(response).to render_template :show
         expect(response.content_type).to eq("text/csv")
-        expect(response.status).to be 200
+        check_status response
       end
     end
 
     context "when the format is json" do
       it "should return json data" do
-        get :show,:id => homer.id, :format => "json"
+        get :show, :id => homer.id, :format => "json"
         expect(response.content_type).to eq("application/json")
-        expect(response.status).to be 200
+        check_status response
+      end
+    end
+  end
+
+  describe "GET transfers" do
+
+    let!(:transfers) do
+      [
+        FactoryGirl.create(:transfer_weekly, :user => homer),
+        FactoryGirl.create(:transfer_monthly, :user => homer)
+      ]
+    end
+
+    context "when a transfer date is in the params" do
+      it "should only return transfers occurring on that date" do
+        date = DateTime.current
+        allow(homer.transfer_calculator).to receive(:transfers_occurring_on) { transfers }
+        get :transfers, :id => homer.id, :transfer => date.to_s, :format => "json", :xhr => true
+        expect(response.content_type).to eq("application/json")
+        # to_json is the json_string, not the JSON object - dandy!
+        expect(response.body).to eq transfers.to_json(:methods => :recurrence)
+        check_status response
       end
     end
 
+    context "when no data is sent in the params" do
+      it "should return all transfers" do
+        homer.transfers = transfers
+        get :transfers, :id => homer.id, :format => "json", :xhr => true
+        expect(response.body).to eq transfers.to_json(:methods => :recurrence)
+        check_status response
+      end
+    end
   end
+
+  def check_status(response, status = 200)
+    expect(response.status).to eq status
+  end
+
 end
