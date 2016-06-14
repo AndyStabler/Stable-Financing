f6706d82cc149149c87ec533216be10fc10c9b0b
Andy Stabler <andy_60@hotmail.co.uk>
2016-06-14 22:23:24 +0100

    Call recurrence correctly from factory spec

--- spec/models/transfer_factory_spec.rb
+++ spec/models/transfer_factory_spec.rb
@@ -6,28 +6,28 @@ RSpec.describe TransferFactory, type: :model do
     context "when the recurrence is a daily recurrence" do
       it "should return a TransferDaily" do
         transfer = FactoryGirl.build(:transfer_daily)
-        expect(TransferFactory.build(TransferDaily.recurrence, transfer.attributes).class).to eq TransferDaily
+        expect(TransferFactory.build(TransferDaily::RECURRENCE, transfer.attributes).class).to eq TransferDaily
       end
     end
 
     context "when the recurrence is a weekly recurrence" do
       it "should return a TransferWeekly" do
         transfer = FactoryGirl.build(:transfer_weekly)
-        expect(TransferFactory.build(TransferWeekly.recurrence, transfer.attributes).class).to eq TransferWeekly
+        expect(TransferFactory.build(TransferWeekly::RECURRENCE, transfer.attributes).class).to eq TransferWeekly
       end
     end
 
     context "when the recurrence is a monthly recurrence" do
       it "should return a TransferMonthly" do
         transfer = FactoryGirl.build(:transfer_monthly)
-        expect(TransferFactory.build(TransferMonthly.recurrence, transfer.attributes).class).to eq TransferMonthly
+        expect(TransferFactory.build(TransferMonthly::RECURRENCE, transfer.attributes).class).to eq TransferMonthly
       end
     end
 
     context "when the recurrence is a no recurrence" do
       it "should return a TransferNoRecurrence" do
         transfer = FactoryGirl.build(:transfer_no_recurrence)
-        expect(TransferFactory.build(TransferNoRecurrence.recurrence, transfer.attributes).class).to eq TransferNoRecurrence
+        expect(TransferFactory.build(TransferNoRecurrence::RECURRENCE, transfer.attributes).class).to eq TransferNoRecurrence
       end
     end
 
