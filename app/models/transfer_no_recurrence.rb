8657568274282a172019c490b84a062197d302db
Andy Stabler <andy_60@hotmail.co.uk>
2016-06-18 15:46:23 +0100

    Return the date (not the transfer)

--- app/models/transfer_no_recurrence.rb
+++ app/models/transfer_no_recurrence.rb
@@ -2,7 +2,7 @@ class TransferNoRecurrence < Transfer
 
   def forecast(from = nil, to)
     from ||= on.to_date
-    (from..to).include?(on.to_date) ? [self] : []
+    (from..to).include?(on.to_date) ? [self.on.to_date] : []
   end
 
   def self.recurrence
